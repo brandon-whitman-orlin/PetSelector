f55be9a88dd8dee3e7da27f7216c646747eee712
brandon-whitman-orlin <brandonwhitmanorlin@gmail.com>
2023-07-04 17:24:22 -0400

    Loading Screen

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    hideLoadingScreen(document.getElementById("loadingScreen"));
+    // showLoadingScreen(document.getElementById("loadingScreen"));
+    // hideLoadingScreen(document.getElementById("loadingScreen"));
+});
+
+function showLoadingScreen(dialog) {
+    dialog.showModal();
+
+    dialog.style.display = "flex";
+
+    const texts = [
+        "Loading",
+        "Loading.",
+        "Loading..",
+        "Loading..."
+    ];
+    let currentIndex = 0;
+    const loadingText = dialog.querySelector("#loadingText");
+
+    setInterval(() => {
+        loadingText.textContent = texts[currentIndex];
+        currentIndex = (currentIndex + 1) % texts.length;
+    }, 250);
+}
+
+function hideLoadingScreen(dialog) {
+    dialog.close();
+
+    dialog.style.display = "none";
+}
